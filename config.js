911033e56e78debb3f5b982e33d8c0abab1dba52
yobbymnare2-sudo <yobbymnare2@gmail.com>
2026-02-17 01:13:26 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    PREFIX: '.',
+    MAX_RETRIES: 3,
+    OWNER_NUMBER: '263714757857',
+    BOT_NAME: 'WhatsApp Web',
+    BOT_VERSION: '1.0.2',
+    IMAGE_PATH: 'https://files.catbox.moe/f9gwsx.jpg',
+};
